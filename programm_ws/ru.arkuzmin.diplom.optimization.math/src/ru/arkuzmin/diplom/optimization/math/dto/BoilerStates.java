190222c7f736578f42aebeef0c6959040204edb9
ArKuzmin <KuzminArtY@gmail.com>
2014-04-20 14:19:35 +0400

    Построение всех возможных режимов работы котлов

--- programm_ws/ru.arkuzmin.diplom.optimization.math/src/ru/arkuzmin/diplom/optimization/math/dto/BoilerStates.java
+++ programm_ws/ru.arkuzmin.diplom.optimization.math/src/ru/arkuzmin/diplom/optimization/math/dto/BoilerStates.java
@@ -6,17 +6,19 @@ package ru.arkuzmin.diplom.optimization.math.dto;
  *
  */
 public enum BoilerStates {
-	UNKNOWN(-1, "Не определено"),
-	OFF(0, "Выключен"), 
-	ON_GAS(1, "Работает на газе"),
-	ON_MAZ(2, "Работает на мазуте");
+	UNKNOWN(-1, "Не определено", "НЕОПР."),
+	OFF(0, "Выключен", "0"), 
+	ON_GAS(1, "Работает на газе", "Г"),
+	ON_MAZ(2, "Работает на мазуте", "М");
 	
 	private int state;
 	private String name;
+	private String str;
 	
-	BoilerStates(int state, String name) {
+	BoilerStates(int state, String name, String str) {
 		this.state = state;
 		this.name = name;
+		this.str = str;
 	}
 	
 	public String getName() {
@@ -25,6 +27,20 @@ public enum BoilerStates {
 	
 	public int getState() {
 		return state;
+	}
+	
+	public static BoilerStates get(int i) {
+		for (BoilerStates bs : values()) {
+			if (bs.state == i) {
+				return bs;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return str;
 	}
 
 }
